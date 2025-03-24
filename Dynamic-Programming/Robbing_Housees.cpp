8caff249720c418b4fb3cf2c3a774686439c1d15
Manik <abhimanik101@gmail.com>
2025-03-24 15:59:15 +0530

    corrected the code

--- Dynamic-Programming/Robbing_Housees.cpp
+++ Dynamic-Programming/Robbing_Housees.cpp
@@ -13,12 +13,12 @@ const ll LINF = 1e18;
 #define debug(x) cout << #x << " = " << (x) << endl;
 #define print_vec(v) for (auto x : v) cout << x << " "; cout << endl;
 //? using tabulation
-int max_house(vectpr<int> &house,int n){
-    vector<int> dp(n);
+int max_house(vector<int> &house,int n,vector<int> & dp){
     if(n == 0) return 0;
     if(n == 1) return house[0];
+    
     dp[0] = house[0];
-    dp[1] = max(dp[0],house[1]);
+    dp[1] = max(house[0],house[1]);
     for(int i = 2;i<n;i++){
         dp[i] = max(dp[i-1] , dp[i-2] + house[i]);
     }
@@ -29,9 +29,11 @@ int max_house(vectpr<int> &house,int n){
 // Main function
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    int n;
+    int n;cin>>n;
     vector<int> house(n);
+    vector<int> dp(n);
     for(auto & x: house) cin>>x;
-    cout<<"The maximum ammount the robber can steal is :"<<max_house(house,n)<<endl;
+    cout<<"The maximum ammount the robber can steal is :"<<max_house(house,n,dp)<<endl;
+    print_vec(dp);
     return 0;
 }
